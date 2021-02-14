c4035f767f8cdcbe78de3fff57d955159e2e909f
meritamm <merita@ttu.ee>
2021-02-14 11:36:07 +0200

    Add project files.

---
+++ Repositories/InMemoryItemsRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemoryItemsRepository : IItemsRepository
+    {
+        public readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potions", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public void CreateItem(Item item)
+        {
+            items.Add(item);
+        }
+
+        public void UpdateItem(Item item)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == item.Id);
+            items[index] = item;
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            items.RemoveAt(index);
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.SingleOrDefault(item => item.Id == id);
+        }
+    }
+}
